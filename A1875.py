affc6eca546b71026aedb9650ca041606dfa2efb
Aly Soliman <alysoliman05@gmail.com>
2023-09-30 20:18:08 +0300

    A1875

---
+++ A1875.py
@@ -0,0 +1,22 @@
+
+  
+n = int(input())
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    arr = list(map(int, input().split()))
+    count = 0 
+    arr.sort()
+    for i in range(c):
+        if (b + arr[i]) <= a:
+            b += arr[i]
+        else:
+            count += (b-1)
+            b = min(1 + arr[i], a)
+    count += b
+    print(count)
+    
+    
+  
+
+
+

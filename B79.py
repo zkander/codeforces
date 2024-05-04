4d9dbe4c67019b75859e094df0ae544f6b33d569
Aly Soliman <alysoliman05@gmail.com>
2024-05-05 01:31:27 +0300

    B79

--- B79.py
+++ B79.py
@@ -1,19 +1,16 @@
 n = int(input())
 
+
 def sum(n):
     sum = 0
     for i in str(n):
         sum += int(i)
     return sum
 
-count = 0 
-while(len(str(n)) > 1):
+
+count = 0
+while (len(str(n)) > 1):
     n = sum(n)
     count += 1
 
 print(count)
-    
-
-
-
- 
